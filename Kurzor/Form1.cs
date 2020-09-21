eb836ae0408a8d6295eeeed1c3c474fad0fd8cdf
Attirobert <karobatti@gmail.com>
2020-09-21 19:30:34 +0200

    Add project files.

---
+++ Kurzor/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kurzor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int n = e.X;
+            int m = e.Y;
+
+            lblSzoveg.Text = ($"X:{n}, Y:{m}");
+            lblSzoveg.Top = m;
+            lblSzoveg.Left = n;
+        }
+    }
+}
